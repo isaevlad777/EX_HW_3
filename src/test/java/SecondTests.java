862babdbe50b3978eafcae3f0e7267a61c136acb
Shokoladka <vladislav.isaev@renue.ru>
2025-03-07 19:05:51 +0400

    1

--- src/test/java/SecondTests.java
+++ src/test/java/SecondTests.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.By.id;
@@ -22,6 +23,17 @@ public class SecondTests {
         $(id("firstName")).setValue("Vladislav");
         $(id("lastName")).setValue("Isaev");
         $(id("userEmail")).setValue("test@test.ru");
+        $(byText("Male")).click();
+        $(id("userNumber")).setValue("9270232999");
+        $(id("dateOfBirthInput")).click();
+        $(".react-datepicker__month-select").click();
+        $(byText("September")).click();
+        $(".react-datepicker__year-select").click();
+        $(byText("1997")).click();
+        $(".react-datepicker__day--018").click();
+        $(id("subjectsInput")).setValue("Tests Subject");
+        $(byText("Music")).click();
+
     }
 
 
