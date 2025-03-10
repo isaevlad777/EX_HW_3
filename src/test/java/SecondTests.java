c0fb2bc25839e6c636be7e19006e207a99d4758f
Shokoladka <vladislav.isaev@renue.ru>
2025-03-11 03:54:08 +0400

    1

--- src/test/java/SecondTests.java
+++ src/test/java/SecondTests.java
@@ -2,10 +2,14 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byText;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.xpath;
 
 public class SecondTests {
 
@@ -31,8 +35,20 @@ public class SecondTests {
         $(".react-datepicker__year-select").click();
         $(byText("1997")).click();
         $(".react-datepicker__day--018").click();
-        $(id("subjectsInput")).setValue("Tests Subject");
+        $(id("subjectsInput")).setValue("C");
+        $(id("react-select-2-option-4")).click();
         $(byText("Music")).click();
+        $("input#uploadPicture").uploadFile(new File("src/test/resources/test.jpg"));
+        $(id("currentAddress")).setValue("Test street and house");
+        $(byText("Select State")).click();
+        $(id("react-select-3-option-3")).click();
+        $(byText("Select City")).click();
+        $(id("react-select-4-option-1")).click();
+        $(id("firstName")).pressEnter();
+
+        $(xpath("//td[text()='Student Name']/following::td[1]")).shouldHave(text("Vladislav Isaev"));
+
+        //$(byTagAndText("td", "Student Name"));
 
     }
 
